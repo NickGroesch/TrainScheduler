36e8773d477e25ef25363877bc1041f7689784d7
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2019-03-18 21:08:44 -0500

    why won't this basic one display? authentication registration issues?

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,25 +12,26 @@ $(document).ready(function() {
   var dB = firebase.database();
 
   // WORKING ON USER AUTHENTICATION~vv
-  uiConfig = {
-    signInSuccessUrl: "<url-to-redirect-to-on-success>",
-    signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID
-    ],
-    // tosUrl and privacyPolicyUrl accept either url string or a callback
-    // function.
-    // Terms of service url/callback.
-    tosUrl: "<your-tos-url>",
-    // Privacy policy url/callback.
-    privacyPolicyUrl: function() {
-      window.location.assign("<your-privacy-policy-url>");
-    }
-  };
-  var ui = new firebaseui.auth.AuthUI(firebase.auth());
-  // The start method will wait until the DOM is loaded.
-  ui.start("#firebaseui-auth-container", uiConfig);
+  // uiConfig = {
+  //   signInSuccessUrl:
+  //     "https://nickgroesch.github.io/authenticationTrainScheduler",
+  //   signInOptions: [
+  //     // Leave the lines as is for the providers you want to offer your users.
+  //     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  //     firebase.auth.GithubAuthProvider.PROVIDER_ID
+  //   ],
+  //   // tosUrl and privacyPolicyUrl accept either url string or a callback
+  //   // function.
+  //   // Terms of service url/callback.
+  //   tosUrl: "<your-tos-url>",
+  //   // Privacy policy url/callback.
+  //   privacyPolicyUrl: function() {
+  //     window.location.assign("<your-privacy-policy-url>");
+  //   }
+  // };
+  // var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  // // The start method will wait until the DOM is loaded.
+  // ui.start("#firebaseui-auth-container", uiConfig);
   // WORKING ON USER AUTHENTICATION~^^
 
   //   Thanks to tutor Brian Ngobidi for walking me through the global flags and firebase reference keys required for the (update details /remove train) functionality
